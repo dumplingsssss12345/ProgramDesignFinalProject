65a074b32e42670668a11cc6534ccaaaff7c7740
dumplingsssss12345 <wuppp554@gmail.com>
2025-04-30 21:07:33 +0800

    地圖無限循環

---
+++ map_system.c
@@ -0,0 +1,99 @@
+#include "raylib.h"
+#include "map_system.h"
+
+//外部變量 玩家位置
+extern Vector2 player;
+
+static Texture2D islandmap = {0};
+static Texture2D grassland = {0};
+static Texture2D volcano = {0};
+static bool islandLoaded = false;
+static bool grasslandLoaded = false;
+static bool volcanoLoaded = false;
+static const int textureSize = 1024;
+
+
+
+void map_is_island() {
+    //加載紋理1次
+    if (!islandLoaded) {
+        islandmap = LoadTexture("resources/map/island.png");
+        islandLoaded = true;
+    }
+
+    
+
+    int centerX = (int)(player.x / textureSize);
+    int centerY = (int)(player.y / textureSize);
+
+    // 畫4*4個格子圍繞玩家
+    for (int i = -2; i <= 1; i++) {
+        for (int j = -2; j <= 1; j++) {
+            int drawX = (centerX + i) * textureSize;
+            int drawY = (centerY + j) * textureSize;
+            DrawTexture(islandmap, drawX, drawY, WHITE);
+        }
+    }
+}
+
+void map_is_grassland() {
+    //加載紋理1次
+    if (!grasslandLoaded) {
+        grassland = LoadTexture("resources/map/grassland.png");
+        grasslandLoaded = true;
+    }
+
+    int centerX = (int)(player.x / textureSize);
+    int centerY = (int)(player.y / textureSize);
+
+    // 畫4*4個格子圍繞玩家
+    for (int i = -2; i <= 1; i++) {
+        for (int j = -2; j <= 1; j++) {
+            int drawX = (centerX + i) * textureSize;
+            int drawY = (centerY + j) * textureSize;
+            DrawTexture(grassland, drawX, drawY, WHITE);
+        }
+    }
+}
+
+void map_is_volcano() {
+    //加載紋理1次
+    if (!volcanoLoaded) {
+        volcano = LoadTexture("resources/map/volcano.png");
+        volcanoLoaded = true;
+    }
+
+    int centerX = (int)(player.x / textureSize);
+    int centerY = (int)(player.y / textureSize);
+
+    // 畫4*4個格子圍繞玩家
+    for (int i = -2; i <= 1; i++) {
+        for (int j = -2; j <= 1; j++) {
+            int drawX = (centerX + i) * textureSize;
+            int drawY = (centerY + j) * textureSize;
+            DrawTexture(volcano, drawX, drawY, WHITE);
+        }
+    }
+}
+
+// 清除暫存中的紋理
+void map_cleanup_islandmap() {
+    if (islandLoaded) {
+        UnloadTexture(islandmap);
+        islandLoaded = false;
+    }
+}
+
+void map_cleanup_grassland() {
+    if (grasslandLoaded) {
+        UnloadTexture(grassland);
+        grasslandLoaded = false;
+    }
+}
+
+void map_cleanup_volcano() {
+    if (volcanoLoaded) {
+        UnloadTexture(volcano);
+        volcanoLoaded = false;
+    }
+}
---
+++ map_system.h
@@ -0,0 +1,6 @@
+void map_is_island();
+void map_is_grassland();
+void map_is_volcano();
+void map_cleanup_islandmap();
+void map_cleanup_grassland();
+void map_cleanup_volcano();
